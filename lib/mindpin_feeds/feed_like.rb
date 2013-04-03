b994fdc0fcfc989257eed93d8f92d01d23270014
fushang318 <fushang318@gmail.com>
2013-04-03 09:14:57 +0800

    add validates

--- lib/mindpin_feeds/feed_like.rb
+++ lib/mindpin_feeds/feed_like.rb
@@ -3,6 +3,7 @@ module MindpinFeeds
     attr_accessible :user, :feed
 
     validates :user, :feed, :presence => true
+    validates :user_id, :uniqueness => {:scope => :feed_id}
 
     belongs_to :user
     belongs_to :feed
