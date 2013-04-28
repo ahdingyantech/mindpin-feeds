dfb8c487ac8e5a1ba2c455a0bdb5b4308e471798
fushang318 <fushang318@gmail.com>
2013-04-28 17:12:13 +0800

    fix

--- lib/mindpin_feeds/instance_methods.rb
+++ lib/mindpin_feeds/instance_methods.rb
@@ -2,7 +2,7 @@ module MindpinFeeds
   module InstanceMethods
     def self.included(base)
       if !base.instance_methods.include?(:creator)
-        base.define_method :creator do
+        base.send(:define_method,:creator) do
           self.user
         end
       end
